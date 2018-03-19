e738a618042e0133089e4f61827d83fd8402cfe5
justam92 <justamrzyg92@gmail.com>
2018-03-19 21:35:51 +0100

    made page for partfolio of landscape architecture in HTML, improve for this CSS, added in JSON images and titles of projects and create tags by JavaScript

---
+++ js/onload_page-portfolio-of-landscape-architecture.js
@@ -0,0 +1,73 @@
+var firstPageAndTitleOfProject = getFirstPageAndTitleOfProject();
+
+
+var containerOfArticle = document.getElementsByTagName("article")[0]; //pobiera z html'a tag article
+
+var referenceToTheLink = document.createElement("a"); //tworzy <a></a>
+referenceToTheLink.href = "#";
+
+var mainScanOfProject = document.createElement("img");
+mainScanOfProject.classList = "mainScanOfProject";
+mainScanOfProject.src = firstPageAndTitleOfProject[0].image;
+
+var titleOfProject = document.createElement("p");
+titleOfProject.classList = "titleOfProject";
+titleOfProject.textContent = firstPageAndTitleOfProject[0].title;
+
+referenceToTheLink.appendChild(mainScanOfProject); //wrzuca <img> do <a></a>
+
+referenceToTheLink.appendChild(titleOfProject);
+
+containerOfArticle.appendChild(referenceToTheLink);
+
+function getFirstPageAndTitleOfProject() {
+    return [{
+        image: "../imgs/portfolio/2015/The concept of land development at the Reda river in Wejherowo/Reda1.jpg",
+        title: "KONCEPCJA ZAGOSPODAROWANIA TERENU PRZY RZECE REDZIE OD UL. IWASZKIEWICZA DO UL. KONOPNICKIEJ W WEJHEROWIE"
+    },
+    {
+        image: "../imgs/portfolio/2015/Engineering project/EP1.jpg",
+        title: "KONCEPCJA ZAGOSPODAROWANIA TERENU PRZY PARAFII RZYMSKO-KATOLICKIEJ P. W. ŚW. KAROLA BOROMEUSZA W WEJHEROWIE"
+    },
+    {
+        image: "../imgs/portfolio/2014/Pavilion project/Pavilion1.jpg",
+        title: "PROJEKT PAWILONU 'POD ŻAGLEM'"
+    },
+    {
+        image: "../imgs/portfolio/2014/Patio project/Patio1.jpg",
+        title: "KONCEPCJA ZAGOSPODAROWANIA WIRYDARZU KLASZTORNEGO W GDAŃSK OLIWA"
+    },
+    {
+        image: "../imgs/portfolio/2014/Green area development project in Gdańsk/GAD1.jpg",
+        title: "PROJEKT ZAGOSPODAROWANIA TERENÓW ZIELENI W GDAŃSKU"
+    },
+    {
+        image: "../imgs/portfolio/2014/Garden lighting project/Garden_lighting1.jpg",
+        title: "PROJEKT OŚWIETLENIA DZIAŁKI W BORKOWIE"
+    },
+    {
+        image: "../imgs/portfolio/2014/Flowerbed project/Flowerbed1.jpg",
+        title: "KONCEPCJA ZAGOSPODAROWANIA TERENU POD RABATĘ W CIENIU"
+    },
+    {
+        image: "../imgs/portfolio/2014/Carpet flower design/Carpet_flower1.jpg",
+        title: "KONCEPCJA KWIETNIKA DYWANOWEGO"
+    },
+    {
+        image: "../imgs/portfolio/2013/VIP lounge/VIP lounge.jpg",
+        title: "PROJEKT SALON DLA VIPA W BORKOWIE: MIEJSCE SPOTKAŃ I WYPOCZYNKU"
+    },
+    {
+        image: "../imgs/portfolio/2013/Dolny Wrzeszcz/DW1.jpg",
+        title: "KONCEPCJA ZAGOSPODAROWANIA PRZESTRZENI ZIELENI W DOLNYM WRZESZCZU"
+    },
+    {
+        image: "../imgs/portfolio/2013/Bench design/BD1.jpg",
+        title: "MEBEL MIEJSKI INSPIROWANY MODERNISTYCZNYM KSZTAŁTEM DOMU BAWEŁNY W GDYNI"
+    },
+    {
+        image: "../imgs/portfolio/2012/Tajnego Hufca Harcerzy's square in Gdynia/square1.jpg",
+        title: "SKWER IM. TAJNEGO HUFCA HARCERZY W GDYNI"
+    }
+    ]
+};
